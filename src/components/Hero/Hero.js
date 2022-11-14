3936cc25e1420a62a7b65f28f5ff652e508e8b38
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-14 22:30:58 +0100

    create Hero component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import HeroContent from '../HeroContent/HeroContent'
+import './hero.css'
+
+function Hero() {
+    return (
+        <div className="hero">
+            <HeroContent />
+        </div>
+    )
+}
+
+export default Hero
