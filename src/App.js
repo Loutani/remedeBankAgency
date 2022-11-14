51c6cda9ce4b6994286374f64889ec73f42d0402
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-14 21:15:10 +0100

    integrate the routes with its pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Sign from "./pages/Sign";
+import User from "./pages/User";
+
 function App() {
     return (
-        <>app</>
+        <>
+          {/* navbar */}
+
+          {/* 
+            routes
+              /
+              /sign 
+              /user
+          */}
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" end element={<Home />} />
+              <Route path="/sign" end element={<Sign />} />
+              <Route path="/user" end element={<User />} />
+              <Route path="/error" end element={<Error />} />
+            </Routes>
+          </BrowserRouter>
+          {/* outlet */}
+
+          {/* footer */}
+        </>
     );
 }
 
