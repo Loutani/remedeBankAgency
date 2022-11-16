730e4291e38d53c7019ac492bbd45f0f8fe3975f
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-16 22:00:23 +0100

    create Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,12 @@
+import './../assets/css/layout/_error.css';
+
 function Error() {
     return (
-        <div>Error</div>
+        <div className="main">
+            <p className="error-code">ERROR <span>404</span></p>
+            <p className="error-description">Something went wrong</p>
+            <p className="redirect">Go to <a href="/">Home</a> page</p>
+        </div>
     )
 }
 
