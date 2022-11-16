2c1c3fa737bc8870b7954c6746dd806a343ef010
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-16 21:50:06 +0100

    create transactions mocked data

---
+++ src/__mock__/transactions.js
@@ -0,0 +1,22 @@
+const transactions = [
+    {
+        id: 't0001',
+        title: 'Argent Bank Checking (x8349)',
+        amount: 2082.79,
+        description: 'Available Balance'
+    },
+    {
+        id: 't0002',
+        title: 'Argent Bank Savings (x6712)',
+        amount: 10928.42,
+        description: 'Available Balance'
+    },
+    {
+        id: 't0003',
+        title: 'Argent Bank Credit Card (x8349)',
+        amount: 184.30,
+        description: 'Current Balance'
+    },
+]
+
+export default transactions;
