e5729be89be38df1213f99dcbed5c1f58719b8bb
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-26 20:14:16 +0100

    integrate value and onchange properties

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,10 +1,10 @@
 import './textInput.css'
 
-function TextInput({id, labelText}) {
+function TextInput({id, labelText, value, onchange}) {
     return (
         <div className="input-wrapper">
             <label htmlFor={id}>{labelText}</label>
-            <input type="text" id={id} />
+            <input type="text" id={id} value={value} onChange={onchange} />
         </div>
     )
 }
