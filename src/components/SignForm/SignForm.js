f931f70b4f087fd0b231283d6b887bd4b94d399b
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-26 20:14:38 +0100

    integrate the username and password state with form submit

--- src/components/SignForm/SignForm.js
+++ src/components/SignForm/SignForm.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import CheckBoxInput from '../CheckBoxInput/CheckBoxInput'
 import SignFormHeader from '../SignFormHeader/SignFormHeader'
 import SubmitButton from '../SubmitButton/SubmitButton'
@@ -5,19 +6,37 @@ import TextInput from '../TextInput/TextInput'
 import './signForm.css'
 
 function SignForm() {
+    const [userName, setUserName] = useState('');
+
+    const [password, setPassword] = useState('');
+
+    const handleUserNameChange = e => {
+        setUserName(e.target.value)
+    }
+
+    const handlePasswordChange = e => {
+        setPassword(e.target.value)
+    }
+
+    const handleFormSubmit = e => {
+        e.preventDefault();
+
+        console.table([userName,password])
+    }
+
     return (
         <section className="sign-in-content">
 
             <SignFormHeader />
 
             <form>
-                <TextInput id='username' labelText='Username' />
+                <TextInput onchange={handleUserNameChange} value={userName} id='username' labelText='Username' />
 
-                <TextInput id='password' labelText='Password' />
+                <TextInput onchange={handlePasswordChange} value={password} id='password' labelText='Password' />
 
                 <CheckBoxInput id='remember-me' labelText='Remember me' />
 
-                <SubmitButton link='/user' text='Sign In' />
+                <SubmitButton link='/#' text='Sign In' onclick={handleFormSubmit}/>
             </form>
         </section>
     )
