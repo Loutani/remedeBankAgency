6a87a0342c30cffa44e6095760f7483b35567df5
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-26 22:30:08 +0100

    create readProfile service to read user profile

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -43,4 +43,25 @@ export const updateProfile = async (profile, token) => {
     return data;
 }
 
+/**
+ * read user profile 
+ * 
+ * @param {string} token 
+ * @returns {Promise}
+ */
+export const readProfile = async (token) => {
+    const response = await fetch('http://localhost:3001/api/v1/user/profile', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(token)
+    });
+
+    const data = await response.json();
+
+    return data;
+}
+
 export default loginService
