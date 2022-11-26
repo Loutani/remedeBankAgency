c154f248d2e1c2f2c872d4d3a7aaabd649913278
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-26 21:59:39 +0100

    create userSlice

---
+++ src/utils/userSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        authenticated: false,
+        user: {},
+        jwt: ''
+    },
+    reducers: {
+      insertProfile: (state, action) => {
+        return {...state, user: action.payload}
+      },
+    },
+  });
+  
+  // this is for dispatch
+  export const { insertProfile } = userSlice.actions;
+  
+  // this is for configureStore
+  export default userSlice.reducer;
