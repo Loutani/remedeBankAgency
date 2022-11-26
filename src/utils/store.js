cb91c86bdb96208e45c9feae4e86606de1a84723
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-26 22:02:06 +0100

    fix store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { useReducer } from "react";
+import userReducer from "./userSlice";
 
 export default configureStore({
 	reducer: {
-		user: useReducer
+		user: userReducer
 	},
 });
