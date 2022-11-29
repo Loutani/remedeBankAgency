ba22638fa1340c5ddaf05d7a6b559dde7519f2de
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-29 23:37:30 +0100

    implement logout feature

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,29 @@
 import { faRightFromBracket, faUserCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { removeUserData } from '../../utils/localStorageHelper'
+import { logout } from '../../utils/userSlice'
 import NavBarItem from '../NavBarItem/NavBarItem'
 import NavBarLogo from '../NavBarLogo/NavBarLogo'
 import './navbar.css'
 
 function NavBar({isLoggedIn}) {
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
+    const handleLogout = (e) => {
+        e.preventDefault()
+
+        dispatch(logout());
+
+        removeUserData();
+
+        navigate('/');
+    }
+
     return (
         <nav className="main-nav">
             <NavBarLogo />
@@ -23,7 +41,7 @@ function NavBar({isLoggedIn}) {
                             Tony
                         </NavBarItem>
 
-                        <NavBarItem link='/'>
+                        <NavBarItem link='#' onclick={handleLogout}>
                             <FontAwesomeIcon icon={faRightFromBracket} />
                             Sign Out
                         </NavBarItem>
