c1bec88af3167b4bf97be97c213ae6519865a08e
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-29 23:03:00 +0100

    make user page use redux selector and fetch profile selector

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,17 +1,41 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { UserLoginContext } from '../App';
 import Header from '../components/Header/Header';
 import Transaction from '../components/Transaction/Transaction';
+import { readProfile } from '../services/loginService';
+import { readUserData, updateUserData } from '../utils/localStorageHelper';
+import { updateProfile } from '../utils/userSlice';
 import transactions from '../__mock__/transactions';
 
 function User() {
     const {setIsLoggedIn} = useContext(UserLoginContext);
 
+    const user = readUserData();
+
+    const dispatch = useDispatch();
+
+    const userState = useSelector(state => state.user)
+
+    useEffect(() => {
+        readProfile(user.jwt).then(response => {
+            if(response.status === 200) {
+                //update user profil redux
+                dispatch(updateProfile(response.body))
+
+                //update user profile in localstorage
+                updateUserData({...user, user: response.body});
+            }else{
+                //redirect to error page
+            }
+        }).catch(err => console.log(err));
+    }, [user.jwt, dispatch])
+
     setIsLoggedIn(true);
 
     return (
         <main className="main bg-dark">
-            <Header />
+            <Header firstName={userState.user.firstName} lastName={userState.user.lastName} />
 
             <h2 className="sr-only">Accounts</h2>
 
