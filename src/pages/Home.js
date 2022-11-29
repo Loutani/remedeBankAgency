cdade5a36be43a22c2f7fb4e4a74fa98b10fffeb
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-29 23:22:12 +0100

    make Home page use lcoalstorage if we have it

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Features from '../components/Features/Features';
 import Hero from '../components/Hero/Hero';
+import { readUserData } from '../utils/localStorageHelper';
+import { insertJWT, updateProfile } from '../utils/userSlice';
 
 function Home() {
+    const user = readUserData();
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(user !== null) {
+            dispatch(insertJWT(user.jwt))
+            dispatch(updateProfile(user.user))
+        }
+    }, [user]);
     return (
         <main>
             <Hero />
