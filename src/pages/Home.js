beba035c5c1d1cc7ff54f3c4ef480745881645f2
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-29 23:16:38 +0100

    use Selector to get user authenticated or no

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,7 @@
-import { useContext } from 'react';
-import { UserLoginContext } from '../App';
 import Features from '../components/Features/Features';
 import Hero from '../components/Hero/Hero';
 
 function Home() {
-    const {setIsLoggedIn} = useContext(UserLoginContext);
-
-    setIsLoggedIn(false);
-    
     return (
         <main>
             <Hero />
