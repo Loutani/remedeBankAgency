1e681c7c0857c0c98beeff7711319c8bb19ee6d9
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-29 23:15:47 +0100

    make NavBarLogo compoent use Link instead of hyperlink

--- src/components/NavBarLogo/NavBarLogo.js
+++ src/components/NavBarLogo/NavBarLogo.js
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom'
 import argentBankLogo from './../../assets/images/argentBankLogo.png'
 import './navBarLogo.css'
 
 function NavBarLogo() {
+
     return (
-        <a className="main-nav-logo" href="/">
+        <Link to='/' className='main-nav-logo'>
             <img className="main-nav-logo-image" src={argentBankLogo} alt="Argent Bank Logo" />
             <h1 className="sr-only">Argent Bank</h1>
-        </a>
+        </Link>
     )
 }
 
