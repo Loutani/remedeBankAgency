481edd3353f1812f74259d57e7f79370a3ad6e2a
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-29 23:37:02 +0100

    add onclick event for NavbarItem component

--- src/components/NavBarItem/NavBarItem.js
+++ src/components/NavBarItem/NavBarItem.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './navBarItem.css'
 
-function NavBarItem({link, children}) {
+function NavBarItem({link, children, onclick = () => {}}) {
     return (
-        <NavLink to={link} className='main-nav-item' >
+        <NavLink to={link} className='main-nav-item' onClick={onclick}>
             {children}
         </NavLink>
     )
