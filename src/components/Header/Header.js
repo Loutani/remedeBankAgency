e9d7d7e8d96c67aa6fce9d102ae2ca9ccde89de2
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-29 23:02:11 +0100

    make Header component use react props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,13 @@ import Button from '../Button/Button'
 import FullName from '../FullName/FullName'
 import './header.css'
 
-function Header() {
+function Header({firstName, lastName}) {
     return (
         <div className="header">
             <h1>
                 Welcome back
                 <br />
-                <FullName userFullName='Tony Jarvis' />!
+                <FullName userFullName={{firstName, lastName}} />!
             </h1>
 
             <Button text='Edit Name' classname='edit-button' />
