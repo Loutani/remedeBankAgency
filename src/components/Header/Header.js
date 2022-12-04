2bd1fba3b1f97386c9becd01923ef5824ff5895b
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-12-04 21:38:29 +0100

    integrate the edit full name user design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Button from '../Button/Button'
 import FullName from '../FullName/FullName'
 import './header.css'
 
-function Header({firstName, lastName}) {
+function Header({firstName = '', lastName = ''}) {
+    const [editForm, setEditForm] = useState(false)
+    const firstNameRef = useRef();
+    const lastNameRef = useRef();
+
+    const handleShowEditForm = () => {
+        setEditForm(editForm => !editForm)
+    }
+
     return (
         <div className="header">
             <h1>
                 Welcome back
                 <br />
-                <FullName userFullName={{firstName, lastName}} />!
+                {
+                    !editForm && <FullName userFullName={{firstName, lastName}} />
+                }
+
+                {
+                    editForm && 
+                    <div>
+                        <form>
+                            <div className='full-name-edit-container'>
+                                <input ref={firstNameRef} type="text" placeholder={firstName} />
+                                <input ref={lastNameRef} type="text" placeholder={lastName} />
+                            </div>
+
+                            <div className='full-name-edit-buttons'>
+                                <Button text='Save' classname='' />
+                                <Button text='Cancel' classname='' />
+                            </div>
+                        </form>
+                    </div>
+                }
             </h1>
 
-            <Button text='Edit Name' classname='edit-button' />
+            {
+                !editForm && <Button text='Edit Name' classname='edit-button' onclick={handleShowEditForm} />
+            }
         </div>
     )
 }
