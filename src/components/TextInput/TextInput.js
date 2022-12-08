321fe46e12b7358718d2ef3fef132be23d1ae674
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-12-08 22:30:53 +0100

    add input type prop to TextInput component

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,10 +1,10 @@
 import './textInput.css'
 
-function TextInput({id, labelText, value, onchange}) {
+function TextInput({id, type = 'text', labelText, value, onchange}) {
     return (
         <div className="input-wrapper">
             <label htmlFor={id}>{labelText}</label>
-            <input type="text" id={id} value={value} onChange={onchange} />
+            <input type={type} id={id} value={value} onChange={onchange} />
         </div>
     )
 }
